23dd837dc9ea2c2c0493d7cbdf50d33214a628a9
IvanDLPG-Pesol <ivan.delapoza@pesol.es>
2024-02-01 09:46:59 +0100

    INIT 12.0

---
+++ delivery_viaxpress/__manifest__.py
@@ -0,0 +1,44 @@
+###############################################################################
+#
+#    SDi Digital Group
+#    Copyright (C) 2021-Today SDi Digital Group <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    "name": "Delivery ViaXpress",
+    "summary": "Delivery Carrier implementation for ViaXpress",
+    "version": "12.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/SidooSL/viaxpress/tree/12.0/delivery_viaxpress",
+    "author": "Sidoo S.L.",
+    "license": "AGPL-3",
+    "images": ['static/description/banner.png'],
+    "application": False,
+    "installable": True,
+    "depends": [
+        "delivery",
+        "delivery_package_number",
+        "delivery_state",
+        "stock",
+    ],
+    "external_dependencies": {
+        "python": ["suds"],
+    },
+    "data": [
+        "views/res_company_view.xml",
+        "views/stock_picking_views.xml",
+    ],
+}
---
+++ delivery_viaxpress/readme/CONFIGURE.rst
@@ -0,0 +1,18 @@
+Para configurar el transportista:
+
+#. Diríjase a *Inventario > Configuración > Entrega > Método de envío* y cree uno nuevo.
+#. Seleccione *ViaXpress* como proveedor.
+#. Configure los datos del servicio que ha contratado.
+
+Para configurar el servicio:
+
+#. Acceda a *Ajustes > Usuarios y Compañías > Compañías* y elija la compañía que utilizará el servicio.
+#. En la pestaña *ViaXpress*, complete los datos de acceso al servicio:
+    - ID Cliente
+    - Usuario
+    - Contraseña
+    - Host
+    - Puerto
+
+    Una vez completados todos los datos, guarde los cambios y pulse el botón *Sincronizar con ViaXpress* para comprobar que la configuración es correcta.
+    Este botón también obtendrá los datos que necesite de la API. En caso de fallar, saldrá un mensaje de error.
